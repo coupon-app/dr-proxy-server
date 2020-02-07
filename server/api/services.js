1353229a79adf99a39f3afe740c49fc779376e78
Daniel Rowe <danielrowetech@gmail.com>
2020-02-07 13:52:37 -0800

    Update service objects

--- server/api/services.js
+++ server/api/services.js
@@ -2,11 +2,16 @@ module.exports = [
   {
     entry: '/products',
     output: '/products',
-    url: 'http://35.162.182.188',
+    url: 'http://carousel.duckdns.org',
   },
   {
     entry: '/reviews',
     output: '/reviews',
-    url: 'http://34.216.202.137',
+    url: 'http://service-reviews.duckdns.org',
+  },
+  {
+    entry: '/checkout',
+    output: '/checkout',
+    url: 'http://18.188.34.11:3003',
   },
 ];
